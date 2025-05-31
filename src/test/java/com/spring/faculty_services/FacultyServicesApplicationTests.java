ec053fabcf18d2125d1204222d45f982d02d93dd
K Naveen <naveenkokkiligadda2001@gmail.com>
2025-05-31 14:30:02 +0530

    initial Commit

---
+++ src/test/java/com/spring/faculty_services/FacultyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.faculty_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
